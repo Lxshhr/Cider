365c5eb7e20dd7b9c0a6ab8603fc6b9f6b249a9f
IllusiveAcie <yxshhp@gmail.com>
2026-08-16 23:32:49 +0300

    mod updates and additions

--- kubejs/server_scripts/recipe-util.js
+++ kubejs/server_scripts/recipe-util.js
@@ -58,7 +58,7 @@ function getItemIds(namespaces) {
 
     return itemIds.filter(id => namespaces.includes(id.split(':')[0]));
 }
-// console.log(JSON.stringify(getItemIds(["firmalife"])));
+console.log(JSON.stringify(getItemIds(["firmalife", "thermia", "hotornot"])));
 
 
 /**
@@ -84,6 +84,6 @@ function getRecipeIds(event, namespaces) {
 }
 
 ServerEvents.recipes(event => {
-    // const all = getRecipeIds(event, ["firmalife"]);
-    // console.log(JSON.stringify(all));
+    const all = getRecipeIds(event, ["firmalife", "thermia", "hotornot"]);
+    console.log(JSON.stringify(all));
 })
